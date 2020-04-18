51ba5861ac689a165f3d02dcea20998a377c946a
mc-12 <34718321+amir-mc@users.noreply.github.com>
2020-04-18 10:25:39 -0700

    Create category.php

---
+++ category.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\category;
+use Illuminate\Http\Request;
+use App\Http\Requests\CreateCatReq;
+
+class CategoriesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('Categories.index')->with('cat', category::all());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('Categories.creat');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(CreateCatReq $request)
+    {
+//      $this->validate($request,[
+//          'name'=>'required|unique:categories'
+//      ]);
+
+     // $Category= new category();
+       category::create([
+           'name'=> $request->name
+
+       ]);
+
+       session()->flash('success','don');
+
+       return redirect('/Categories');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(category $Category)
+    {
+
+
+        return view('Categories.creat')->with('Category', $Category);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
