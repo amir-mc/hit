dcde99df999d02dd17da6dbe4d8311accb849475
mc-12 <34718321+amir-mc@users.noreply.github.com>
2020-04-23 10:32:49 -0700

    Update post.php

--- app/post.php
+++ app/post.php
@@ -3,8 +3,10 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class post extends Model
 {
-    //
+       use SoftDeletes;
+    protected $fillable=['title','dis','contents','image','publish_at'];
 }
