68a598794cf485a89110e22777c4ce52f17de29c
mc-12 <34718321+amir-mc@users.noreply.github.com>
2020-04-29 06:25:41 -0700

    Update post.php

--- app/post.php
+++ app/post.php
@@ -2,12 +2,24 @@
 
 namespace App;
 
+use Dotenv\Store\StoreBuilder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Storage;
 
 class post extends Model
 {
-       use SoftDeletes;
-    protected $fillable=['title','dis','contents','image','publish_at'];
+    use SoftDeletes;
+    protected $fillable=['title','dis','contents','image','publish_at','category_id'];
+
+
+    public function deleteimage()
+    {
+        Storage::delete($this->image);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(category::class);
+    }
 }
-//
