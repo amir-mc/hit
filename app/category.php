49294a039f79445342fe873138005508449d6deb
mc-12 <34718321+amir-mc@users.noreply.github.com>
2020-04-29 06:26:11 -0700

    Update category.php

--- app/category.php
+++ app/category.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class category extends Model
 {
-    //
+     protected $fillable=['name'];
+
+    public function post()
+    {
+        return $this->hasMany(post::class);
+    }
 }
-//
