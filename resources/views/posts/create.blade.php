ee9fc66a5553ec177cbe4f875f0e36278eabbd89
mc-12 <34718321+amir-mc@users.noreply.github.com>
2020-05-02 07:25:07 -0700

    Update create.blade.php

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,112 +1,165 @@
-@extends('layouts.app')
-@section('content')
+<?php
 
-    <div class="card card-default">
-    <div class="card-header">
-        {{isset($posts)? 'edit':'creat'}}
-    </div>
+namespace App\Http\Controllers;
 
-    <div class="card-body">
+use App\category;
+use App\Http\Requests\post\postupdateRequest;
+use App\post;
+use App\Tags;
+use Illuminate\Http\Request;
+use App\Http\Requests\post\postRequest;
+use Illuminate\Support\Facades\Storage;
 
-        @include('partials.error')
+class PostsController extends Controller
+{
 
-
-
-
-        <form action="{{isset($posts)?route('posts.update',$posts->id):route('posts.store')}}" method="post" enctype="multipart/form-data">
-            @csrf
-
-            @if(isset($posts))
-                @method('PUT')
-
-            @endif
-        <div class="form-group">
-            <label for="title">title</label>
-            <input type="text" class="form-control" name="title" id="title" value=" {{isset($posts)? $posts->title:''}}">
-        </div>
-            <div class="form-group">
-            <label for="dis">description</label>
-                <textarea name="dis" id="dis" cols="5" rows="5" class="form-control"  >{{isset($posts)? $posts->dis:''}}</textarea>
-        </div>
-            <div class="form-group">
-            <label for="contents">Contents</label>
-                <input id="content" type="hidden" name="content">
-{{--                <trix-editor id="contents"  input="content"> </trix-editor>--}}
-                <textarea name="contents" placeholder="dis" id="contents"  cols="5" rows="5" class="form-control" > {{isset($posts)? $posts->contents:''}}</textarea>
+    public function __construct()
+    {
+        $this->middleware('verifyCatMid')->only(['create','store']);
+    }
 
 
-            </div>
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('posts.index')->with('posts',post::all());
+    }
 
-            <div class="form-group">
-                <label for="category">category</label>
-                <select name="category" class="form-control" id="category">
-                   @foreach($categorys as $Category)
-                    <option value="{{$Category->id}}"
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('posts.create')->with('categorys',category::all())->with('tags',Tags::all());
+    }
 
-                            @if(isset($posts))
-
-                            @if($Category->id == $posts->category_id)
-                            selected
-                        @endif
-
-                        @endif
-
-                    >
-                        {{$Category->name}}
-                    </option>
-                    @endforeach
-
-                </select>
-            </div>
-
-            <div class="form-group">
-                <label for="publish_at">Publish at</label>
-                <input type="date" class="form-control" name="publish_at" id="publish_at"   value="{{isset($posts)? $posts->publish_at:''}}" >
-            </div>
-
-
-{{--            <div class="form-group"> <!-- Date input -->--}}
-
-{{--                <label for="publish_at">Date Of Birth</label>--}}
-{{--                <input type="date" name="publish_at" id="publish_at" value="">--}}
-{{--            </div>--}}
-            @if(isset($posts))
-            <div class="form-group">
-                <img src="{{asset($posts->image)}}">
-            </div>
-
-@endif
-
-            <div class="form-group">
-                <label for="image">image</label>
-                <input type="file" class="form-control" name="image" id="image">
-            </div>
-            <div class="form-group">
-            <button type="submit" class="btn btn-success">{{isset($posts)?'edit':'publish Post'}}</button>
-            </div>
-        </form>
-    </div>
-    </div>
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(postRequest $request)
+    {
+     //  $image=    $request->image->storeas('posts',$request->image->getClientOriginalName());
+     // $image=    $request->image->store('posts',$request->image->getClientOriginalName());
+     //$image=asset('posts/'.$request->image->getClientOriginalName());
+//       dd( $request->image);
+       // $image = $request->file('image')->getClientOriginalName();
 
 
-@endsection
-
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"  crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"  crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/trix/1.2.1/trix-core.js" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/flatpickr" crossorigin="anonymous"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
+      $image =  $request->image->store('posts');
 
 
-<script>
-   // flatpickr('#publish_at')
-</script>
+       $posts= post::create([
+            'title'=>$request->title,
+            'dis'=>$request->dis,
+            'contents'=>$request->contents,
+            'publish_at'=>$request->publish_at,
+            'image'=> $image,
+             'category_id'=>$request->category
+        ]);
+        if ($request->tags){
+            $posts->tag()->attach($request->tags);
+        }
+        session()->flash('success','well don');
+        return redirect(route('posts.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(post $post)
+    {
+        return view('posts.create')->with( 'posts',$post )->with('categorys',category::all())->with('tags',Tags::all());
 
 
-@section('css')
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/trix/1.2.1/trix.css"  crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
-@endsection
 
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(postupdateRequest $request ,post $post)
+    {
+        $data=$request->only(['title','dis','contents','publish_at']);
+
+        if ($request->hasFile('image'))
+        {
+            $image=$request->image->store();
+           $post->deleteimage();
+            $data['image']=$image;
+
+        }
+        if ($request->tags){
+            $post->tag()->sync($request->tags);
+        }
+
+
+       $post->update($data);
+        session()->flash('success','edit don');
+        return redirect(route('posts.index'));
+    }
+
+
+    public function destroy($id)
+    {
+        $post=post::withTrashed()->where('id', $id)->firstOrFail();
+
+      $post->delete();
+
+        if ($post->trashed())
+        {
+            $post->deleteimage();
+            $post->delete();
+        }
+        else{
+            $post->delete();
+        }
+
+        session()->flash('success','well don');
+        return redirect(route('posts.index'));
+
+    }
+
+    //just four trash file
+
+    public function trash()
+    {
+        $trash= post::onlyTrashed()->get();
+        return view('posts.index')->with('posts',$trash);
+    }
+
+    public function restore($id)
+    {
+        $post=post::withTrashed()->where('id', $id)->firstOrFail();
+        $post->restore();
+        session()->flash('success','restore don');
+        return redirect(route('posts.index'));
+    }
+
+}
