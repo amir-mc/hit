cb9e48256eb7e459823357956b7dfb984329a96b
mc-12 <34718321+amir-mc@users.noreply.github.com>
2020-05-02 07:25:31 -0700

    Update index.blade.php

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,89 +1,82 @@
 @extends('layouts.app')
 @section('content')
-    <a href="{{route('posts.create')}}" class="btn btn-info my-2">add post</a>
-    <div class="card card-default">
-        <div class="card-header">
-            post
+    <a href="tags/create" class="btn btn-info my-2">add tags</a>
+<div class="card card-default">
+    <div class="card-header">
+        cat
 
-        </div>
-        <div class="card-body">
-@if($posts->count()>0)
-                <table class="table">
-                    <thead>
-                    <th>
-                        image
-                    </th>
-                    <th>
-                        title
-                    </th>
-                    <th>
-                        category
-                    </th>
-                    </thead>
-
-                    <tbody>
-
-
-
-                    @foreach($posts  as $post)
-
-                        <tr>
-                            <td>
-                                <img src="{{asset($post->image)}}" alt="">
-
-                            </td>
-                            <td>  {{$post->title}}</td>
-                            <td>
-
-                                <a href="{{route('Categories.edit', $post->category->id)}}">
-
-                                    {{$post->category->name}}
-
-                                </a>
-
-
-
-                            </td>
-                            <td>
-                                @if($post->trashed())
-                                    <form action="{{route('post.restore',$post->id)}}" method="post">
-                                        @csrf
-                                        @method('PUT')
-                                    <button type="submit"  class="btn btn-info">restore</button>
-                           </form>
-                            </td>
-                            @else
-
-
-
-
-                         <td>
-                             <a href="{{route('posts.edit',$post->id)}}" class="btn btn-info">   edit</a>
-
-                         </td>
-                            @endif
-                            <td>
-                                <form action="{{route('posts.destroy',$post->id)}}" method="POST">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="btn btn-warning">
-                                        {{$post->trashed()?'delete':'trash'}}
-                                    </button>
-
-                                </form>
-                            </td>
-
-                        </tr>
-                    @endforeach
-@else
-    <h3 class="text-center">empty</h3>
-@endif
-                    </tbody>
-                </table>
-
-
-
-
-        </div>
     </div>
-@endsection
+    <div class="card-body">
+        @if($tag->count()>0)
+        <table class="table">
+            <thead>
+            <th>
+                 name:
+            </th>
+            <th>
+                post:
+            </th>
+            </thead>
+            <tbody>
+            @foreach($tag as $tags)
+            <tr><td>{{$tags->name}}</td>
+                <td>{{$tags->post->count()}}</td>
+
+            <td>
+
+                <a href="{{route('tags.edit', $tags->id)}}" class="btn btn-info">edit</a>
+                <button id="mod"  class="btn btn-warning" onclick="handeldelete({{$tags->id}})">delete</button>
+            </td>
+            </tr>
+            @endforeach
+
+            </tbody>
+        </table>
+@else
+            <h3 class="text-center">empty</h3>
+        <!-- Modal -->
+@endif
+        <form action="" method="POST" id="de">
+
+            @csrf
+            @method('DELETE')
+
+        <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModallabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="deleteModalLabel">delete cat</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        are you sure ??
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-danger">Delete</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </form>
+    </div>
+</div>
+    @endsection
+
+
+
+    <script>
+        function handeldelete(id) {
+
+            var form = document.getElementById('de')
+            form.action='/tags/'+id
+
+            $("#deleteModal").modal('show')
+        }
+    </script>
+
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"  crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"  crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" crossorigin="anonymous"></script>
